3771aa2672bb0830b8ecb2d049637c45fff0515f
William <william.legal03@gmail.com>
2017-03-07 13:58:32 +0100

    up

--- variable.php
+++ variable.php
@@ -1,4 +1,19 @@
 <?php
-$nom = "william";
+$nom = 'william';
 echo $nom;
 ?>
+
+<?php
+$nom = 'legal';
+$prenom = 'william';
+$age = 22;
+echo $nom ,$prenom ,$age;
+?>
+<?php 
+$kilometre = 1;
+echo $kilometre;
+$kilometre = 3;
+echo $kilometre;
+$kilometre= 125;
+echo $kilometre;
+ ?>
