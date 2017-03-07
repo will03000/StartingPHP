6e67a1731d0fc40aacb85914cb71e11bab8fe970
William <william.legal03@gmail.com>
2017-03-07 14:20:50 +0100

    exo orga

--- variable.php
+++ variable.php
@@ -1,28 +1,56 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
 
-<?php
-$nom = 'william';
-echo "$nom </br>";
-?>
+	<div id="exo1">
+		<?php
+		$nom = 'william';
+		echo "$nom </br>";
+		?>
+	</div>
 
-<?php
-$nom = 'legal';
-$prenom = 'william';
-$age = 22;
-echo "$nom </br> $prenom </br> $age </br>";
-?>
-<?php 
-$kilometre = 1;
-echo " $kilometre </br>";
-$kilometre = 3;
-echo "$kilometre </br>";
-$kilometre= 125;
-echo "$kilometre </br>";
- ?>
 
- <?php
- $string = 'boubou';
- $int = 22;
- $float = 3.2345;
- $boolean = true;
- echo "$string </br> $int </br> $float </br> $boolean";
- ?>
+	<div id="exo2">
+		<?php
+		$nom = 'legal';
+		$prenom = 'william';
+		$age = 22;
+		echo "$nom </br> $prenom </br> $age </br>";
+		?>
+	</div>
+
+	<div id="exo3">
+		<?php 
+		$kilometre = 1;
+		echo " $kilometre </br>";
+		$kilometre = 3;
+		echo "$kilometre </br>";
+		$kilometre= 125;
+		echo "$kilometre </br>";
+		 ?>
+	</div>
+
+	<div id="exo4">
+		 <?php
+		 $string = 'boubou';
+		 $int = 22;
+		 $float = 3.2345;
+		 $boolean = true;
+		 echo "$string </br> $int </br> $float </br> $boolean </br>";
+		 ?>
+	</div>
+
+	<div id="exo5">
+		 <?php
+		 $int2 = null;
+		 echo $int2;
+		 $int2 = 5467;
+		 echo "$int2 </br>";
+		 ?>
+	</div>	
+	 
+</body>
+</html>
