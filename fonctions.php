e80653739070722cb3af9e2c7e5da52018257aba
William <william.legal03@gmail.com>
2017-03-08 11:05:50 +0100

    exo4.1

---
+++ fonctions.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+
+	<div>
+		<?php
+		function caca(){
+			return true;
+		}
+
+		?>
+	</div>
+
+</body>
+</html>
--- index.php
+++ index.php
@@ -9,6 +9,7 @@
 	<a href="variable.php">exo1</a>
 	<a href="conditions.php">exo2</a>
 	<a href="boucles.php">exo3</a>
+	<a href="fonctions.php">exo4</a>
 
 
 </body>
