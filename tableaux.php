ea5222b1ed536a0453116b5e8d5b5c4398554240
William <william.legal03@gmail.com>
2017-03-08 14:06:01 +0100

    exo5.7

--- tableaux.php
+++ tableaux.php
@@ -8,15 +8,70 @@
 <div>
 
 	<?php
-	$tab = array('janvier','février','mars','avril','mai','juin','juillet','aôut','septembre','octobre','novembre','décembre');
+	$tab = array('janvier','février','mars','avril','mai','juin','juillet','aout','septembre','octobre','novembre','décembre');
 	print_r($tab);
 	?>
 </div>
 
 <div>
 	<?php
+	$tab = array('janvier','février','mars','avril','mai','juin','juillet','aout','septembre','octobre','novembre','décembre');
+	echo $tab[2];
+	?>
+</div>
+
+<div>
+	<?php
+	$tab = array('janvier','février','mars','avril','mai','juin','juillet','aout','septembre','octobre','novembre','décembre');
+	echo $tab[5];
+	?>
+</div>
+
+<div>
+	<?php
 	$tab = array('janvier','février','mars','avril','mai','juin','juillet','aôut','septembre','octobre','novembre','décembre');
+	$tab[3]= 'julien';
 	echo $tab[3];
+	?>
+</div>
+
+<div>
+	<?php
+	$tabdep = array(
+		59 => 'Nord',
+		02=>'Aisne',
+		80=>'Somme',
+		60=>'Oise',
+		62=>'Pas-de-Calais',
+		);
+	?>
+</div>
+
+<div>
+	<?php
+	$tabdep = array(
+		59 => 'Nord',
+		02=>'Aisne',
+		80=>'Somme',
+		60=>'Oise',
+		62=>'Pas-de-Calais',
+		);
+	echo $tabdep[59];
+	?>
+</div>
+
+<div>
+	<?php
+	
+$tabdep = array(
+		59 => 'Nord',
+		02=>'Aisne',
+		80=>'Somme',
+		60=>'Oise',
+		62=>'Pas-de-Calais',
+		);
+	$tabdep[51]= 'Marne';
+	print_r($tabdep);
 	?>
 </div>
 
