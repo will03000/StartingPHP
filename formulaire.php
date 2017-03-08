460a56f261955ad3794cf0c1f32eec28358b1a74
William <william.legal03@gmail.com>
2017-03-08 16:36:52 +0100

    exo7

--- formulaire.php
+++ formulaire.php
@@ -7,9 +7,10 @@
 
 
 <?php
-if (isset($_GET['nom']) && isset($_GET['prenom']) && isset($_GET['civilité'])) :
+if (isset($_GET['nom']) && isset($_GET['prenom']) && isset($_GET['civilité']) && isset($_GET['fichier'])) :
 ?>
 Bonjour, <?= $_GET['civilité'];?> <?= $_GET['nom']; ?> <?= $_GET['prenom']; ?>
+<?= $_GET['fichier'];?>
 
 <?php else : ?>
 	<form action="users.php" method="get">
@@ -24,13 +25,14 @@ Bonjour, <?= $_GET['civilité'];?> <?= $_GET['nom']; ?> <?= $_GET['prenom']; ?>
  <p><input type="submit" value="OK"></p>
 </form>
 
-<form action="formulaire.php" method="get">
+<form action="formulaire.php" enctype="multipart/form-data" method="get">
   <select name="civilité" size="1">
  	<option>Monsieur</option>
  	<option>Madame</option>
  </select>
  <p>Votre nom : <input type="text" name="nom" /></p>
  <p>Votre prénom : <input type="text" name="prenom" /></p>
+<input type="file" name="fichier"/>
  <p><input type="submit" value="OK"></p>
 </form>
 <? endif; ?>
