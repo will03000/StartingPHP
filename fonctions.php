f5a2ffb5622bc9b6b92d4d440a672ef738b81d09
William <william.legal03@gmail.com>
2017-03-08 11:44:49 +0100

    exo4.5

--- fonctions.php
+++ fonctions.php
@@ -23,5 +23,43 @@
 		?>
 	</div>
 
+	<div>
+	
+		<?php
+		function concat($var1,$var2){
+			
+			return $var1.$var2;
+		}
+		echo concat('bonjour ','moi');
+		?>
+	</div>
+
+	<div>
+	
+		<?php
+		function comparaison($int1,$int2){
+			if ($int1 > $int2) {
+				echo  $int1.' est plus grand';
+			}
+			if ($int1<$int2) {
+				echo $int1.' est plus petit ';
+			}
+			if ($int1==$int2) {
+				echo 'les deux nombres sont identiques';
+			}
+		}
+		echo comparaison(12,24);
+		?>
+	</div>
+
+	<div>
+		<?php
+		function mixte($nombre,$chaine){
+			return $nombre.$chaine;
+		}
+		echo mixte(5,' euros');
+		?>
+	</div>
+
 </body>
 </html>
