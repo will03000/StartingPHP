c9c373a21a0267ffa7104bd3a1fef8778e12a501
William <william.legal03@gmail.com>
2017-03-09 11:18:14 +0100

    exo8.5

--- superglobal2.php
+++ superglobal2.php
@@ -6,7 +6,24 @@
 	echo $_SESSION['age'];
 
 ?>
+
+
 <?php
-echo $_COOKIE['login'];
-echo $_COOKIE['password'];
+if (isset($_POST['login']) && isset($_POST['password'])) {
+		setcookie('login',$_POST['login']);
+		setcookie('password',$_POST['password']);
+	}
 ?>
+
+
+
+	<?php
+if (isset($_COOKIE['login']) && isset($_COOKIE['password'])) : ?>
+
+	<form action="superglobal2.php" method="post">
+		 <p>Votre login: <input type="text" name="login" value="<?= $_COOKIE['login'] ?>" /></p>
+		 <p>Votre password : <input type="text" name="password"  value="<?= $_COOKIE['password'] ?>" /></p>
+		 <p><input type="submit" value="OK"></p>
+	</form>
+
+<?php endif; ?>
