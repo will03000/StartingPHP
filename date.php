648f09c1a39585317408ff6557472e26f6343cd5
William <william.legal03@gmail.com>
2017-03-09 16:30:36 +0100

    exo9

--- date.php
+++ date.php
@@ -15,5 +15,11 @@ echo strftime("%A %d %B %G").'<br>';
 
 <?php 
 echo time().'<br>';
- echo mktime(15, 34, 12, 3, 2, 2016);
+echo mktime(15, 34, 12, 3, 2, 2016).'<br>';
+$datetime1 = new DateTime('2016-05-16');
+$datetime2 = new DateTime();
+$interval = $datetime1->diff($datetime2);
+echo $interval->format('%R%a jours');
+
+
  ?>
