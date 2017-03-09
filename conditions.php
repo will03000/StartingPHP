2de7e602de8aa6bd556ab345d2d9b3f3956416ec
William <william.legal03@gmail.com>
2017-03-09 13:46:18 +0100

    exo8.5

--- conditions.php
+++ conditions.php
@@ -22,7 +22,7 @@
 <div>
 		<?php
 		$iseasy = true;
-		if ($iseasy == false) {
+		if ($iseasy == true) {
 			echo "c'est vrai </br>";
 		}else{
 			echo "c'est difficile </br>";
@@ -32,19 +32,17 @@
 
 <div>
 	<?php
-	$age2 = '';
-	$genre = '';
+	$age2;
+	$genre = 'homme';
 	
-	if ($age2<= 18) {
-		$age2 += 'mineur';
-	}else{
-		$age2+='majeur';
-	}
-	if ($genre = 'homme') {
-		$genre+= 'homme';
-	}else{
-		$genre+='femme';
-		echo "vous un ".$genre."et vous êtes ".$age2.".";
+	if ($age2<= 18 && $genre =='homme') {
+		echo "vous etes un homme mineur";
+	}elseif ($age2 >= 18 && $genre == 'homme') {
+		echo "vous etes un homme majeur";
+	}elseif ($age2 <= 18 && $genre == 'femme') {
+		echo "vous etes une femme  mineur";
+	}elseif ($age2 >= 18 && $genre == 'femme') {
+		echo "vous etes un femme majeur";
 	}
 	
 	?>
@@ -55,31 +53,31 @@
 	$magnitude;
 	switch($magnitude){
 
-		case 0:
+		case "0":
 			echo "Micro-séisme impossible à ressentir.";
 			break;
-		case 1:
+		case "1":
 			echo "Micro-séisme impossible à ressentir mais enregistrable par les sismomètres";
 			break;
-		case 2:
+		case "2":
 			echo "Ne cause pas de dégats mais commence à pouvoir être légèrement ressenti.";
 			break;
-		case 3:
+		case "3":
 			echo "Séisme capable de faire bouger des objets mais ne causant généralement pas de dégats.";
 			break;
-		case 4:
+		case "4":
 			echo "Séisme capable d'engendrer des dégats importants sur de vieux bâtiments ou bien des bâtiments présentants des défauts de construction. Peu de dégats sur des bâtiments modernes.";
 			break;
-		case 5:
+		case "5":
 			echo "Fort séisme capable d'engendrer des destructions majeures sur une large distance (180 km) autour de l'épicentre.";
 			break;
-		case 6:
+		case "6":
 			echo "Séisme capable de destructions majeures à modérées sur une très large zone en fonction de la distance.";
 			break;
-		case 7:
+		case "7":
 			echo "Séisme capable de destructions majeures sur une très large zone de plusieurs centaines de kilomètres.";
 			break;
-		case 8:
+		case "8":
 			echo "Séisme capable de tout détruire sur une très vaste zone.";
 			break;								
 
@@ -89,10 +87,11 @@
 
 <div>
 	<?php
-	if ($maVariable != 'Homme') {
-		echo "'C\'est un développeur !!!'";
-	}else{
+	if ($maVariable2 != 'Homme') {
 		echo "'C\'est une développeuse !!!' ";
+	}else{
+		
+		echo "'C\'est un développeur !!!'";
 	}
 	?>
 </div>
