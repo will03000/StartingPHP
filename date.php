86e303839a45d8b8bc6c415c95a7501cdf25de6a
William <william.legal03@gmail.com>
2017-03-09 15:45:07 +0100

    exo9

--- date.php
+++ date.php
@@ -1,4 +1,16 @@
 <?php
-$date = date("d/m/Y");
+$date = date("d/m/Y").'</br>';
 echo $date;
+?>
+
+<?php
+$date = date('d/m/y').'</br>';
+echo $date;
+?>
+
+<?php
+
+setlocale (LC_TIME, 'fr_FR.utf8','fra'); 
+echo strftime("%A %d %B %G"); 
+
 ?>
